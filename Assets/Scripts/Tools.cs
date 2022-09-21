ecace044a5b8d4db0209d4c0ae431df9e828266e
francois07 <francois.soulie@edu.esiee.fr>
2022-09-22 00:05:23 +0200

    Corrected Spherical coordinates tools

--- Assets/Scripts/Tools.cs
+++ Assets/Scripts/Tools.cs
@@ -4,111 +4,136 @@ using UnityEngine;
 
 public class Tools : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+  // Start is called before the first frame update
+  void Start()
+  {
 
-    }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
+  // Update is called once per frame
+  void Update()
+  {
 
-    }
+  }
 }
 
 namespace MyMathTools
 {
-    [System.Serializable]
-    public struct Polar
+  [System.Serializable]
+  public struct Polar
+  {
+    public float Rho;
+    public float Theta;
+
+    public Polar(Polar pol)
     {
-        public float Rho;
-        public float Theta;
-
-        public Polar(Polar pol)
-        {
-            this.Rho = pol.Rho;
-            this.Theta = pol.Theta;
-        }
-
-        public Polar(float rho, float theta)
-        {
-            this.Rho = rho;
-            this.Theta = theta;
-        }
+      this.Rho = pol.Rho;
+      this.Theta = pol.Theta;
     }
 
-    [System.Serializable]
-    public struct Cylindrical
+    public Polar(float rho, float theta)
     {
-        public float Rho;
-        public float Theta;
-        public float y;
+      this.Rho = rho;
+      this.Theta = theta;
+    }
+  }
 
-        public Cylindrical(Cylindrical cyl)
-        {
-            this.Rho = cyl.Rho;
-            this.Theta = cyl.Theta;
-            this.y = cyl.y;
-        }
+  [System.Serializable]
+  public struct Cylindrical
+  {
+    public float Rho;
+    public float Theta;
+    public float y;
 
-        public Cylindrical(float rho, float theta, float y)
-        {
-            this.Rho = rho;
-            this.Theta = theta;
-            this.y = y;
-        }
+    public Cylindrical(Cylindrical cyl)
+    {
+      this.Rho = cyl.Rho;
+      this.Theta = cyl.Theta;
+      this.y = cyl.y;
     }
 
-    [System.Serializable]
-    public struct Spherical
+    public Cylindrical(float rho, float theta, float y)
     {
-        public float Rho;
-        public float Theta;
-        public float Phi;
+      this.Rho = rho;
+      this.Theta = theta;
+      this.y = y;
+    }
+  }
 
-        public Spherical(Spherical sph)
-        {
-            this.Rho = sph.Rho;
-            this.Theta = sph.Theta;
-            this.Phi = sph.Phi;
-        }
+  [System.Serializable]
+  public struct Spherical
+  {
+    public float Rho;
+    public float Theta;
+    public float Phi;
 
-        public Spherical(float rho, float theta, float phi)
-        {
-            this.Rho = rho;
-            this.Theta = theta;
-            this.Phi = phi;
-        }
+    public Spherical(Spherical sph)
+    {
+      this.Rho = sph.Rho;
+      this.Theta = sph.Theta;
+      this.Phi = sph.Phi;
     }
 
-    public static class CoordConvert
+    public Spherical(float rho, float theta, float phi)
     {
-        public static Vector2 PolarToCartesian(Polar polar)
-        {
-            return polar.Rho * new Vector2(Mathf.Cos(polar.Theta), Mathf.Sin(polar.Theta));
-        }
-
-        public static Polar CartesianToPolar(Vector2 cart, bool keepThetaPositive = true)
-        {
-            Polar polar = new Polar(cart.magnitude, 0);
-            if (Mathf.Approximately(polar.Rho, 0)) polar.Theta = 0;
-            else
-            {
-                polar.Theta = Mathf.Asin(cart.y / polar.Rho);
-                if (cart.x < 0) polar.Theta = Mathf.PI - polar.Theta;
-                if (keepThetaPositive && polar.Theta < 0) polar.Theta += 2 * Mathf.PI;
-            }
-            return polar;
-        }
-
-        public static Vector3 SphericalToCartesian(Spherical sph)
-        {
-            float radius = sph.Rho;
-            float inclination = sph.Theta;
-            float azimuth = sph.Phi;
-
-            return new Vector3(radius * Mathf.Sin(inclination) * Mathf.Cos(azimuth), radius * Mathf.Sin(inclination) * Mathf.Sin(azimuth), radius * Mathf.Cos(inclination));
-        }
+      this.Rho = rho;
+      this.Theta = theta;
+      this.Phi = phi;
     }
+
+    public static Spherical operator +(Spherical s1, Spherical s2)
+    {
+      return new Spherical(s1.Rho + s2.Rho, s1.Theta + s2.Theta, s1.Phi + s2.Phi);
+    }
+  }
+
+  public static class CoordConvert
+  {
+    public static Vector2 PolarToCartesian(Polar polar)
+    {
+      return polar.Rho * new Vector2(Mathf.Cos(polar.Theta), Mathf.Sin(polar.Theta));
+    }
+
+    public static Polar CartesianToPolar(Vector2 cart, bool keepThetaPositive = true)
+    {
+      Polar polar = new Polar(cart.magnitude, 0);
+      if (Mathf.Approximately(polar.Rho, 0)) polar.Theta = 0;
+      else
+      {
+        polar.Theta = Mathf.Asin(cart.y / polar.Rho);
+        if (cart.x < 0) polar.Theta = Mathf.PI - polar.Theta;
+        if (keepThetaPositive && polar.Theta < 0) polar.Theta += 2 * Mathf.PI;
+      }
+      return polar;
+    }
+
+    public static Vector3 SphericalToCartesian(Spherical sph)
+    {
+      float radius = sph.Rho;
+      float inclination = sph.Theta;
+      float azimuth = sph.Phi;
+
+      return new Vector3(radius * Mathf.Cos(azimuth) * Mathf.Cos(inclination), radius * Mathf.Sin(inclination), radius * Mathf.Sin(azimuth) * Mathf.Cos(inclination));
+    }
+
+    public static Spherical CartesianToSpherical(Vector3 car)
+    {
+      float rho = Mathf.Sqrt(car.x * car.x + car.y * car.y + car.z * car.z);
+      float theta;
+      float phi;
+
+      if (rho == 0)
+      {
+        theta = 0;
+        phi = 0;
+      }
+      else
+      {
+        theta = Mathf.Atan2(car.y, car.x);
+        phi = Mathf.Acos(car.z / rho);
+      }
+
+      return new Spherical(rho, theta, phi);
+    }
+  }
 }
