509766de20e27a00007417bd5d0b3185b0ecb70b
francois07 <francois.soulie@edu.esiee.fr>
2022-09-28 21:57:19 +0200

    Created CreateGridXZMesh method & started createCylinderMeshFromStrip method

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MyMathTools;
 
 public class MeshGenerator : MonoBehaviour
 {
@@ -8,7 +9,7 @@ public class MeshGenerator : MonoBehaviour
     void Start()
     {
         MeshFilter meshFilter = GetComponent<MeshFilter>();
-        meshFilter.mesh = createStripXZMesh(new Vector3(8, 0, 8), 8);
+        meshFilter.mesh = createGridXZMesh(new Vector3(8, 0, 8), 8, 8);
     }
 
     // Update is called once per frame
@@ -123,5 +124,92 @@ public class MeshGenerator : MonoBehaviour
         mesh.RecalculateBounds();
 
         return mesh;
+    }
+
+    Mesh createGridXZMesh(Vector3 size, int nSegmentsX, int nSegmentsZ)
+    {
+        Vector3 halfSize = size / 2;
+        int nVertices = (nSegmentsX + 1) * (nSegmentsZ + 1);
+
+        Mesh mesh = new Mesh();
+        mesh.name = "StripXZ";
+        Vector3[] vertices = new Vector3[nVertices];
+        int[] triangles = new int[nSegmentsX * nSegmentsZ * 6];
+        Vector3[] normals = new Vector3[nVertices];
+        Vector2[] uv = new Vector2[nVertices];
+
+        // Vertices generation
+        for (int i = 0; i < nSegmentsZ + 1; i++)
+        {
+            float kZ = halfSize.z + (size.z / nSegmentsZ) * i;
+
+            for (int j = 0; j < nSegmentsX + 1; j++)
+            {
+                float kX = -halfSize.x + (size.x / nSegmentsX) * j;
+                int idx = i * (nSegmentsX + 1) + j;
+
+                vertices[idx] = new Vector3(kX, 0, kZ);
+                uv[idx] = new Vector2(vertices[idx].x, vertices[idx].z);
+                normals[idx] = Vector3.up;
+            }
+        }
+
+        // Create GridXZ Mesh triangles
+        for (int i = 0; i < nSegmentsZ; i++)
+        {
+            for (int j = 0; j < nSegmentsX; j++)
+            {
+                int p0 = j + i * (nSegmentsX + 1);
+                int p1 = p0 + 1;
+                int p2 = p0 + nSegmentsX + 1;
+                int p3 = p2 + 1;
+
+                int idx = (j * 6) + (6 * nSegmentsX) * i;
+
+                triangles[idx] = p0;
+                triangles[idx + 1] = p1;
+                triangles[idx + 2] = p2;
+
+                triangles[idx + 3] = p1;
+                triangles[idx + 4] = p3;
+                triangles[idx + 5] = p2;
+
+                Debug.Log(idx);
+                Debug.Log(i);
+                Debug.Log(j);
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.normals = normals;
+        mesh.uv = uv;
+
+        mesh.RecalculateBounds();
+
+        return mesh;
+    }
+
+    Mesh createCylindricalMeshFromStrip(Vector3 size, int nSegmentsX)
+    {
+        Mesh stripMesh = createStripXZMesh(size, nSegmentsX);
+        Vector3[] vertices = stripMesh.vertices;
+        Vector3 halfSize = size / 2;
+
+        float radius = size.x / (2 * Mathf.PI);
+
+        for (int i = 0; i < vertices.Length / 2; i++)
+        {
+            float theta = ((Mathf.PI * 2) / nSegmentsX) * i;
+            vertices[i] = CoordConvert.CylindricalToCartesion(new Cylindrical(radius, theta, halfSize.y));
+
+            int offset = vertices.Length - i - 1;
+            vertices[offset] = CoordConvert.CylindricalToCartesion(new Cylindrical(radius, theta, -halfSize.y));
+        }
+
+        stripMesh.vertices = vertices;
+        stripMesh.RecalculateBounds();
+
+        return stripMesh;
     }
 }
